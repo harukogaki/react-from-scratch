ad23d38ea572d2cc74f74e4e72e8143b2715f0c4
Salvador Ariza <sariza@thoughtworks.com>
2019-10-22 11:02:07 -0500

    Initial Commit

---
+++ src/Root.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default ({name}) => (
+    <> 
+        <h1>Hello World</h1>
+        <h2>My name is { name} </h2>
+    </>
+);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Root from "./Root"
+import React from "react"
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <Root name="Salvador"/>,
+    document.getElementById('root')
+  );
